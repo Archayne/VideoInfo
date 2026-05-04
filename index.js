384ca18f30a1e241db6bbb847400320e610f56d5
Archayne <hargrovea31@gmail.com>
2026-05-04 11:37:28 -0400

    added webpreferenceand ffmpeg

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const electron = require('electron');
 const { app, BrowserWindow } = electron; //const app = electron.app;
 app.on('ready', ()=>{
     console.log("App is ready");
-    const mainWindow = new BrowserWindow();
+    const mainWindow = new BrowserWindow({
+        webPreferences:{
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
